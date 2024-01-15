b4800c2f6564c1faba9ce232ae5c377cd4036148
yogevgit10 <yogev.git@runi.ac.il>
2024-01-15 22:27:02 +0200

    updated

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,95 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
-    }
-
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int arr = secondMaxValue(new int[] { 6, 9, 4, 7, 3, 4 });
+        System.out.println(arr);
+        boolean condition = containsTheSameElements(new int[] { 6, 1, 4, 1, 3, 7 }, new int[] { 4, 6, 4, 7, 3, 1 });
+        System.out.println(condition);
+        boolean condition2 = isSorted(new int[] { 2, 1, 0, 1, -2 });
+        System.out.println(condition2);
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static int findMissingInt(int[] array) {
         // Write your code here:
-        return false;
+        int count = 0;
+        int missingInt = 0;
+        for (int i = 0; i < array.length; i++) {
+            count = count + array[i];
+        }
+        missingInt = ((array.length) * (array.length + 1)) / 2 - count;
+        return missingInt;
+
     }
 
-    public static boolean isSorted(int [] array) {
+    public static int secondMaxValue(int[] array) {
         // Write your code here:
-        return false;
+        int[] newarray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            newarray[i] = array[i];
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < newarray.length; i++) {
+            if (newarray[i] > max)
+                max = newarray[i];
+        }
+        for (int i = 0; i < newarray.length; i++) {
+            if (newarray[i] == max) {
+                newarray[i] = Integer.MIN_VALUE;
+                max = Integer.MIN_VALUE;
+                break;
+            }
+        }
+        for (int i = 0; i < newarray.length; i++) {
+            if (newarray[i] > max)
+                max = newarray[i];
+        }
+        return max;
+
+    }
+
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    count1++;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array1.length; j++) {
+                if (array2[i] == array1[j]) {
+                    count2++;
+                    break;
+                }
+            }
+        }
+        if ((count1 == array1.length) && (count2 == array2.length))
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean isSorted(int[] array) {
+        int countup = 1;
+        int countdown = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] >= array[i - 1])
+                countup++;
+            else
+                break;
+        }
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] <= array[i - 1])
+                countdown++;
+            else
+                break;
+        }
+        if ((countdown == array.length) || (countup == array.length))
+            return true;
+        else
+            return false;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -1,41 +1,65 @@
 public class StringOps {
     ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
+    ////// ///////
+    ////// Reminder: ///////
+    ////// allowed methods ///////
+    ////// ///////
+    ////// 1.charAt(int index) ///////
+    ////// 2.length() ///////
+    ////// 3.substring(int start) ///////
+    ////// 4.substring(int start,int ends) ///////
+    ////// 5.indexOf(String str) ///////
+    ////// ///////
+    ////// The rest are not allowed ! ///////
+    ////// if you want to use a different ///////
+    ////// method, and you can implement ///////
+    ////// it using material from the course ///////
+    ////// you need to implement a version of ///////
+    ////// the function by yourself. ///////
+    ////// ///////
+    ////// see example for substring ///////
+    ////// in Recitation 3 question 5 ///////
+    ////// ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        System.out.println(capVowelsLowRest("I Love You"));
+
     }
 
-    public static String capVowelsLowRest (String string) {
+    public static String capVowelsLowRest(String str) {
         // Write your code here:
-        return "";
+        String newstr = "";
+        String renewstr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if ((65 <= ((int) (str.charAt(i)))) && (((int) (str.charAt(i))) <= 90)) {
+                newstr = newstr + ((char) (str.charAt(i) + 32));
+            } else
+                newstr = newstr + ((char) (str.charAt(i)));
+        }
+        for (int i = 0; i < newstr.length(); i++) {
+            if (newstr.charAt(i) == 'a')
+                renewstr = renewstr + ((char) (newstr.charAt(i) - 32));
+            else if (newstr.charAt(i) == 'e')
+                renewstr = renewstr + ((char) (newstr.charAt(i) - 32));
+            else if (newstr.charAt(i) == 'i')
+                renewstr = renewstr + ((char) (newstr.charAt(i) - 32));
+            else if (newstr.charAt(i) == 'o')
+                renewstr = renewstr + ((char) (newstr.charAt(i) - 32));
+            else if (newstr.charAt(i) == 'u')
+                renewstr = renewstr + ((char) (newstr.charAt(i) - 32));
+            else
+                renewstr = renewstr + newstr.charAt(i);
+        }
+        return renewstr;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        int i = 0;
+        for (int i = 0; i <= n;)
+            return "";
     }
 
-    public static int[] allIndexOf (String string, char chr) {
+    public static int[] allIndexOf(String string, char chr) {
         // Write your code here:
         return new int[1];
     }
