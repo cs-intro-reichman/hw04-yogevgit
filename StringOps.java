fff91063acbf664a9a182577da51758b0c1e9eb8
yogevgit10 <yogev.git@runi.ac.il>
2024-01-17 13:16:32 +0200

    YOGEV

--- StringOps.java
+++ StringOps.java
@@ -22,6 +22,13 @@ public class StringOps {
     ////// ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String sting = "One two tHRee world";
+        System.out.println(capVowelsLowRest(sting));
+        System.out.println(camelCase(sting));
+        int[] arr = allIndexOf(sting, 'o');
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
     }
 
